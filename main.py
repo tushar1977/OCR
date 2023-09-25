1ee712125b2062acd1fc3c63fb067e638b95dc22
tushar1977 <36126341+tushar1977@users.noreply.github.com>
2023-09-25 10:17:16 +0530

    Add files via upload
    UPDATED NOW IT CAN STORE WORDS IN FILE

--- main.py
+++ main.py
@@ -4,16 +4,25 @@ import pytesseract
 
 #defining global variables
 pytesseract.pytesseract.tesseract_cmd = 'D:\\Tesseract-OCR\\tesseract.exe'
-img = cv2.imread('2.png')
+img = cv2.imread('1.png')
 
 def pre_process(img):
 
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     return img
+
+def print_words(box_2):
+    for b in box_2.splitlines():
+        b = b.split('\t')
+
+        with open('test.txt', 'a') as f:
+            f.writelines(b[-1]+" ")
 def detecting_words(img_new):
 
     himg, wimg, _ = img_new.shape
     boxes = pytesseract.image_to_boxes(img_new)
+    box_2 = pytesseract.image_to_data(img_new)
+    print_words(box_2)
 
     for b in boxes.splitlines():
         b = b.split(' ')
@@ -22,9 +31,12 @@ def detecting_words(img_new):
         cv2.putText(img_new, b[0], (x, himg - y + 25), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255))
 
 
-img = pre_process(img)
-detecting_words(img)
+
+img_2 = pre_process(img)
+detecting_words(img_2)
+#rint_word(img_2)
 
 
-cv2.imshow('Result', img)
+
+cv2.imshow('Result', img_2)
 cv2.waitKey(0)
