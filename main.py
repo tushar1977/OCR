e6ba42a52ac66e2f8d0f0d452899c059b76b2eb6
tushar1977 <36126341+tushar1977@users.noreply.github.com>
2023-09-25 10:22:09 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,24 +1,20 @@
 import cv2
 import pytesseract
 
-
 #defining global variables
-pytesseract.pytesseract.tesseract_cmd = 'D:\\Tesseract-OCR\\tesseract.exe' #please give the location to your own pytessaract file location
+pytesseract.pytesseract.tesseract_cmd = 'D:\\Tesseract-OCR\\tesseract.exe'
 img = cv2.imread('1.png')
 
 def pre_process(img):
 
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     return img
-
 def print_words(box_2):
-    
     for b in box_2.splitlines():
         b = b.split('\t')
 
         with open('test.txt', 'a') as f:
             f.writelines(b[-1]+" ")
-            
 def detecting_words(img_new):
 
     himg, wimg, _ = img_new.shape
@@ -32,9 +28,11 @@ def detecting_words(img_new):
         cv2.rectangle(img_new, (x, himg - y), (w, himg - h), (0, 0, 255), 1)
         cv2.putText(img_new, b[0], (x, himg - y + 25), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255))
 
+def main():
 
-img_2 = pre_process(img)
-detecting_words(img_2)
+    img_2 = pre_process(img)
+    detecting_words(img_2)
+    #rint_word(img_2)
 
-cv2.imshow('Result', img_2)
-cv2.waitKey(0)
+    cv2.imshow('Result', img_2)
+    cv2.waitKey(0)
